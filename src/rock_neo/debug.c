dfbab1c877e3f181068ee1aba9bd85f3210150e0
Christian <chrisnonymous2000@gmail.com>
2023-03-21 11:40:10 -0600

    found a new TU

---
+++ src/rock_neo/debug.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_800629E0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_800629F0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80062A50);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80062B04);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80062C6C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80062CF0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80063058);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80063334);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_80063604);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/debug", func_800638D8);
