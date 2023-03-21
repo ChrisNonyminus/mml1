d0c77bf9a58cfe53f242b2c7c92bc2c6f0a1a981
Christian <chrisnonymous2000@gmail.com>
2023-03-21 12:05:10 -0600

    scene.c

---
+++ src/rock_neo/scene.c
@@ -0,0 +1,119 @@
+#include "common.h"
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D854);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D878);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D888);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D8C0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D928);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D974);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001D990);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", Sce_flag_test);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DA8C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DCD0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DD88);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DDC0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DDE4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DE84);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DEDC);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DEE4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DF10);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001DFEC);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E068);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E20C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E390);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E3F0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E460);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E4C4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E550);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E6C0);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E7E4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E810);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E83C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001E968);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001EAE8);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001EB98);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001EC0C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F070);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F158);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F1DC);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F20C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F23C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F3C8);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F580);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F5E4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F6C4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F740);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F798);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F820);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F828);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F8DC);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001F9AC);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FA94);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FB24);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FB54);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FB8C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FBC4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FC50);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FCA4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FCE4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FD3C);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FD90);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/scene", func_8001FDE4);
