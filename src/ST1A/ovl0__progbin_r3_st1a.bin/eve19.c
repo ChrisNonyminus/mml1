6612afd15b07726a507ebb28f95006ac3131812d
Christian <chrisnonymous2000@gmail.com>
2023-03-23 16:47:58 -0600

    found eve19.c

---
+++ src/ST1A/ovl0__progbin_r3_st1a.bin/eve19.c
@@ -0,0 +1,23 @@
+#include "common.h"
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012B920);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012B95C);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012B97C);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012BB54);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012BBEC);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012BD4C);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012BEC4);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012C03C);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012C1B4);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012C1C0);
+
+INCLUDE_ASM("config/overlay/splat.us.ST1A/../../../asm/ST1A/ovl0__progbin_r3_st1a.bin/nonmatchings/eve19", func_8012C228);
